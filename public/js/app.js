488bdd1124937feda5ff1effd4cf33598f33d92b
ObywatelTB <obywateltb@gmail.com>
2020-07-23 01:34:05 +0200

    tworzenie szablonu strony.

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+//client-side java script
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+//messageOne.textContent = 'From javascript'
+
+
+weatherForm.addEventListener('submit',(e)=>{
+	e.preventDefault() //browser wont refresh
+	
+	const location = search.value
+	
+	messageOne.textContent = 'loading...'
+	messageTwo.textContent = ''
+	
+	fetch('/weather?address='+location).then((response)=>{
+		response.json().then((data)=>{
+			if(data.error){
+				messageOne.textContent = data.error
+			}else{
+				messageOne.textContent = data.location
+				messageTwo.textContent = data.forecast
+			}
+		})
+	})
+})
+
