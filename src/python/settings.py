1d5b2ac3e73b1edb9c5de1d50d77997319b07f69
Mateusz Morawiec <mateusz.f.morawiec@gmail.com>
2020-08-23 16:09:59 +0200

    python - sciezki plikow dodawane z liku dev.env

---
+++ src/python/settings.py
@@ -0,0 +1,10 @@
+# settings.py
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(__file__).parents[1] / 'config/dev.env'
+load_dotenv(dotenv_path=env_path)
+
+import os
+SECRET_KEY = os.getenv("PYTHON_LIBRARIES")
